1af084b580425d874391c185c653942c382e85ff
unknown <longdhit@gmail.com>
2018-11-23 19:33:59 +0700

    update them account

--- src/views/pageUser/User.js
+++ src/views/pageUser/User.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
-import { Table, Button } from 'antd';
+import { Table, Button, Input, InputNumber, Form } from 'antd';
 import { compose } from 'redux';
-import { firestoreConnect } from 'react-redux-firebase';
+import { firestoreConnect, withFirestore } from 'react-redux-firebase';
 import { setExpert, unsetExpert } from '../../reduxModules/admin/adminActions';
+
 import './User.scss';
+/* eslint-disable */
+const FormItem = Form.Item;
+const EditableContext = React.createContext();
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
+
+const EditableFormRow = Form.create()(EditableRow);
+
+class EditableCell extends React.Component {
+  getInput = () => {
+    if (this.props.inputType === 'number') {
+      return <InputNumber />;
+    }
+    return <Input />;
+  };
+
+  render() {
+    const {
+      editing,
+      dataIndex,
+      title,
+      inputType,
+      record,
+      index,
+      ...restProps
+    } = this.props;
+    return (
+      <EditableContext.Consumer>
+        {(form) => {
+          const { getFieldDecorator } = form;
+          return (
+            <td {...restProps}>
+              {editing ? (
+                <FormItem style={{ margin: 0 }}>
+                  {getFieldDecorator(dataIndex, {
+                    rules: [{
+                      required: true,
+                      message: `Please Input ${title}!`,
+                    }],
+                    initialValue: record[dataIndex],
+                  })(this.getInput())}
+                </FormItem>
+              ) : restProps.children}
+            </td>
+          );
+        }}
+      </EditableContext.Consumer>
+    );
+  }
+}
 
 class User extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // data: []
+      editingKey: ''
     };
-  }
-  render() {
-    const columns = [
+    this.columns = [
       {
         title: 'Id',
         dataIndex: 'id',
@@ -29,6 +81,7 @@ class User extends Component {
       {
         title: 'Tài khoản expert',
         dataIndex: 'account',
+        editable: true,
         key: 'account'
       },
       {
@@ -45,22 +98,95 @@ class User extends Component {
       },
       {
         title: 'Hành động',
-        dataIndex: 'action',
-        render: (text, user) =>
-          user.role === 'expert' ? (
-            <Button onClick={() => this.props.unsetExpert(user.id)} type="primary" >
+        dataIndex: 'operation',
+        render: (text, record) => {
+          const editable = this.isEditing(record);
+          return (
+            <div>
+              
+              {editable ? (
+                <span>
+                  <EditableContext.Consumer>
+                    {form => (
+              
+                      <Button onClick={() => this.save(form, record)}
+                      style={{ marginRight: 8 }} type="primary" >
+                        Save
+                  </Button>
+                    )}
+                  </EditableContext.Consumer>
+                  <Button style={{ marginRight: 8 }} onClick={() => this.cancel(record.key)} type="primary" >
+                  Cancel
+              </Button>
+                </span>
+              ) : (
+                <Button style={{ marginRight: 8 }} onClick={() => this.edit(record.key)} type="primary" >
+                Edit
+              </Button>
+              )}
+              {record.role === 'expert' ? (
+            <Button onClick={() => this.props.unsetExpert(record.id)} type="primary" >
               Unset Expert
             </Button>
           ) : (
-            <Button onClick={() => this.props.setExpert(user.id)} type="primary">
+            <Button onClick={() => this.props.setExpert(record.id)} type="primary">
               Set Expert
             </Button>
-          ),
-        key: 'action'
+          )}
+            </div>
+          );
+        },
       }
     ];
+  }
+  isEditing = (record) => {
+    return record.key === this.state.editingKey;
+  };
+
+  edit(key) {
+    this.setState({ editingKey: key });
+  }
+  cancel = () => {
+    this.setState({ editingKey: '' });
+  };
+  save(form, key) {
+    const { firestore } = this.props
+    form.validateFields((error, row) => {
+      if (error) {
+        return;
+      }
+      const { account } = row;
+      const { id } = key;
+      firestore.update({ collection: 'users', doc: id }, {account});
+      this.setState({ editingKey: '' });
+    });
+  }
+
+  render() {
+    const components = {
+      body: {
+        row: EditableFormRow,
+        cell: EditableCell,
+      },
+    };
+
+    const columns = this.columns.map((col) => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          inputType: col.dataIndex === 'age' ? 'number' : 'text',
+          dataIndex: col.dataIndex,
+          title: col.title,
+          editing: this.isEditing(record),
+        }),
+      };
+    });
     return (
-      <div> <Table dataSource={this.props.users} bordered columns={columns} /></div>
+      <div> <Table components={components} dataSource={this.props.users} bordered columns={columns} rowClassName="editable-row" /></div>
     );
   }
 }
@@ -76,4 +202,4 @@ export default compose(
     }
   ),
   firestoreConnect(['users'])
-)(User);
+)(withFirestore(User));
