7376bce952ebeba0e61b6773e4e37fa77a4e60fe
unknown <longdhit@gmail.com>
2018-11-28 15:23:25 +0700

    up

--- src/views/components/Navigation/Navigation.js
+++ src/views/components/Navigation/Navigation.js
@@ -30,13 +30,13 @@ class Navigation extends React.Component {
             inlineCollapsed={collapsed}
           >
             {
-              role === 'expert' && <Menu.Item key={'expert/' + uid}><Icon type="bar-chart" /><span>Trang Cá Nhân</span></Menu.Item>
+              role === 'expert' && <Menu.Item key={'expert/' + uid}><Icon type="bar-chart" /><span>Khu Vực Chuyên Gia</span></Menu.Item>
             }
             {
               role === 'expert' && <Menu.Item key="signal/expert"><Icon type="bar-chart" /><span>Room Tín Hiệu</span></Menu.Item>
             }
             {
-              role === 'member' && <Menu.Item key="dashboard"><Icon type="home" /><span>Dashboard</span></Menu.Item>
+              role === 'member' && <Menu.Item key="dashboard"><Icon type="home" /><span>Trang Tổng Quan</span></Menu.Item>
             }
             {
               role === 'member' && <Menu.Item key="experts"><Icon type="bar-chart" /><span>Danh Sách Chuyên Gia</span></Menu.Item>
@@ -44,13 +44,12 @@ class Navigation extends React.Component {
             {
               role === 'member' && <Menu.Item key="signals"><Icon type="bar-chart" /><span>Room Tín Hiệu</span></Menu.Item>
             }
+           {
+              role === 'expert' &&  <Menu.Item key="account"><Icon type="bar-chart" />Nền tảng giao dịch</Menu.Item>
+                }
             {isAuthenticated && 
               <Menu.SubMenu className="nav-item" title={<span><Icon type="user" /><span>Quản Lý</span></span>}>
                 <Menu.Item key="information">Thông Tin Cá Nhân</Menu.Item>
-                {
-              role === 'expert' &&  <Menu.Item key="account">Thông Tin Tài Khoản</Menu.Item>
-                }
-               
                 <Menu.Item key="changepassword">Đổi Mật Khẩu</Menu.Item>
               </Menu.SubMenu>
             }
