c3442a23e2eb0b75042ac2c88472480edefee7f5
unknown <longdhit@gmail.com>
2018-12-14 22:51:31 +0700

    up

--- src/vendor.js
+++ src/vendor.js
@@ -1,7 +1,7 @@
-import "axios";
-import "prop-types";
-import "react";
-import "react-dom";
-import "redux";
-import "redux-thunk";
-import "react-router-dom";
+// import "axios";
+// import "prop-types";
+// import "react";
+// import "react-dom";
+// import "redux";
+// import "redux-thunk";
+// import "react-router-dom";
